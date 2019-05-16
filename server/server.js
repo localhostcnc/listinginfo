15b033f392c1d0c5bcb632c7b2f5b3fd0390edc4
Connor Homan <connorhoman14@gmail.com>
2019-05-16 13:08:32 -0700

    Fixed server issues, now up and running

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3006;
 
-app.use(express.static('./client/'));
+app.use(express.static('client/'));
 
 app.get('/', (req, res) => {
   console.log('Recieved GET request');
