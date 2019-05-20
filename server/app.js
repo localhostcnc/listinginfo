f9bcce03fadc76f39dfdddbb4455a6c59b108c02
Connor Homan <connorhoman14@gmail.com>
2019-05-20 16:15:53 -0700

    Added server tests via supertest

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const db = require('../database/queries.js');
+
+const app = express();
+app.use(bodyparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(express.static('public/'));
+
+app.get('/listing_info', db.getInfo);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const db = require('../database/queries.js');
+const app = require('./app');
 
-const app = express();
 const port = 3006;
-
-app.use(bodyparser());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-
-app.use(express.static('public/'));
-
-app.get('/listing_info', db.getInfo);
 
 app.listen(port, (err) => {
   if (err) {
