567bf46fb453c9af03dd5a04a8410613ddefac55
Connor Homan <connorhoman14@gmail.com>
2019-05-27 10:43:01 -0700

    Finalized data

--- database/seed.js
+++ database/seed.js
@@ -19,7 +19,6 @@ const names = ['Lake Almanor Lake Front Retreat',
   'Beachfront Villa Near Boardwalk',
   'Cute Quirky Garden apt, NYC adjacent',
   'CLOSEST GRAND CANYON B&B/South Rim',
-  'The Cabin',
   'Take in a Dip in the Heated Pool at a Luxury SoCo Retreat'];
 const ownerTypes = ['Verified', 'none', 'Plus'];
 const responseTypes = ['weeks', 'hours', 'months'];
@@ -34,14 +33,8 @@ const descriptions = ['This is a quaint cabin tucked in the trees on the east sh
   'Prime location with parking. Walking distance to UCLA. Minutes from Santa Monica, Venice, Hollywood and other tourist attractions. Full kitchen with appliances, A/C unit, and Wi-Fi.The spaceIf you are visiting Los Angeles and looking to be in a prime location within close proximity to the citys touristic attractions and famous beaches, my renovated studio in the heart of Westwood is the choice for you.My unit is on the third (top floor) of the building so there is a beautiful skyline view from the three large windows. These windows bring lots of bright natural light, but if you choose to cover them each window has a retractable shade. There is also a skylight in the kitchen and bathroom ceilings for more natural light.I have a queen sized bed (med-firm foam mattress) which sleeps two people comfortably. If you have a third guest, I can also provide an extra single sleeper bed upon request.There is a full kitchen complete with stainless steel appliances, dish ware and utensils if you would like to prepare your own meals.There is also a medium sized desk and wi-fi available for use if you need to get any work done during your stay.I have an in-unit AC/Heater, but you are welcome to open the windows if you wish.I provide one assigned parking space. The space may be tight for larger vehicles.The UCLA campus is 1.1 miles away- 6 to 7 minute car ride- 8 minute bike ride- 18 minute UCLA Blue Bus ride- 25 minute walkGuest accessGuests will be able to utilize the entire studio apartment privately.Interaction with guestsPlease feel free to shoot me a message prior.',
   'The space. Una habitación doble en un apartamento tranquilo situada en un edificio histórico que data de la época medieval. A pocos metros se situan el Museo Picasso, boutiques independientes, galerías de arte, talleres de artistas y gran número de edificios representativos de Barcelona. Todo ello en la zona del Borne conocido por la gran cantidad de bares y restaurantes de moda que contiene.Cuando el clima lo permita se puede disfrutar en la terraza privada, que por supuesto estará a disposición de los huéspedes durante su estancia, resulta un lugar ideal para leer un libro o tomar una copa de vino después de un día de turismo.Varias estaciones de metro y paradas de autobuses están muy cerca, sin embargo la mayoría de las atracciones turísticas se encuentran a corta distancia, la catedral, Santa Maria del Mar, las playas y las Ramblas se encuentran a menos de 10 minutos.El Vaixell de papel (El barco de papel) - quiere significar algo tan frágil y al mismo tiempo capaz de una gran fortaleza!Other things to notePlease note that there will be a €40 charge payable in cash for check-ins later than 23:00pm',
 ];
-
-// const amenitieslist = ['Beach essentials', 'Beach towels', 'Umbrella', 'Beach blanket', 'Snorkeling gear', 'Cable TV', 'Dryer', 'Essentials',
-//   'Towels, bed sheets, soap, and toilet paper', 'Heating', 'Central heating or a heater in the listing', 'Hot water', 'Iron', 'TV', 'Washer', 'Wifi', 'Free parking on premises',
-//   'Dining', 'Coffee maker', 'Cooking basics', 'Pots and pans, oil, salt and pepper', 'Dishes and silverware', 'Dishwasher', 'Kitchen', 'Space where guests can cook their own meals',
-//   'Microwave', 'Refrigerator', 'Stove', 'Guest access', 'Private entrance', 'Separate street or building entrance', 'Bed and bath', 'Bed linens',
-//   'Extra pillows and blankets', 'Hair dryer', 'Hangers', 'Outdoor', 'BBQ grill', 'Beachfront', 'Lake access', 'Safety features', 'Fire extinguisher',
-//   'Carbon monoxide detector', 'Smoke detector', 'First aid kit',
-// ];
+const cancellations = ['Strict - Free cancellation for 48 hours', 'After that, cancel up to 7 days before check-in and get a 50% refund, minus the service fee.'];
+const rules = ['Not safe or suitable for infants (under 2 years) and pets', 'No smoking, parties, or events', 'Check-in is anytime after 4PM and check out by 12PM (noon)', 'Shoes off at door', 'No outside guests'];
 
 for (let i = 0; i < 100; i += 1) {
   const name = names[Math.floor(Math.random() * names.length)];
@@ -63,21 +56,17 @@ for (let i = 0; i < 100; i += 1) {
   const description2 = descriptions[Math.floor(Math.random() * descriptions.length)];
   const description3 = descriptions[Math.floor(Math.random() * descriptions.length)];
   const amenities = Math.floor(Math.random() * 40);
-  // for (let j = 0; j < 15; j += 1) {
-  //   amenities += amenitieslist[Math.floor(Math.random() * amenitieslist.length)];
-  //   amenities += ' ';
-  // }
-  const location1 = faker.lorem.sentence();
-  const location2 = faker.lorem.sentence();
-  const location3 = faker.lorem.sentence();
-  const rules1 = faker.lorem.sentence();
-  const rules2 = faker.lorem.sentence();
-  const rules3 = faker.lorem.sentence();
-  const rules4 = faker.lorem.sentence();
-  const cancellation1 = faker.lorem.sentence();
-  const cancellation2 = faker.lorem.sentence();
-  const cancellation3 = faker.lorem.sentence();
-  const cancellation4 = faker.lorem.sentence();
+  const location1 = descriptions[Math.floor(Math.random() * descriptions.length)].slice(0, 100);
+  const location2 = descriptions[Math.floor(Math.random() * descriptions.length)].slice(0, 100);
+  const location3 = descriptions[Math.floor(Math.random() * descriptions.length)].slice(0, 100);
+  const rules1 = rules[Math.floor(Math.random() * rules.length)];
+  const rules2 = rules[Math.floor(Math.random() * rules.length)];
+  const rules3 = rules[Math.floor(Math.random() * rules.length)];
+  const rules4 = rules[Math.floor(Math.random() * rules.length)];
+  const cancellation1 = cancellations[0];
+  const cancellation2 = cancellations[Math.floor(Math.random() * cancellations.length)];
+  const cancellation3 = rules[Math.floor(Math.random() * rules.length)];
+  const cancellation4 = rules[Math.floor(Math.random() * rules.length)];
   const query = `INSERT INTO listing_info (name, city, avatar, owner, ownerType, guests, beds, rooms, baths, bedType1, bedType2, bedType3, bedType4, responseRate, responseType, description1, description2, description3, amenities, location1, location2, location3, rules1, rules2, rules3, rules4, cancellation1, cancellation2, cancellation3, cancellation4) VALUES ('${name}', '${city}', '${avatar}', '${owner}', '${ownertype}', ${guests}, ${beds}, ${rooms}, ${baths}, '${bedType1}', '${bedType2}', '${bedType3}', '${bedType4}', ${responserate}, '${responseType}', '${description1}', '${description2}', '${description3}', ${amenities}, '${location1}', '${location2}', '${location3}', '${rules1}', '${rules2}', '${rules3}', '${rules4}', '${cancellation1}', '${cancellation2}', '${cancellation3}', '${cancellation4}')`;
 
   pool.query(query, (err) => {
