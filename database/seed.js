4d23f911c368cfbff30c5ec791065c0187e97c27
Connor Homan <connorhoman14@gmail.com>
2019-06-01 17:29:41 -0700

    Using container name as host

--- database/seed.js
+++ database/seed.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'connorhoman',
-  host: '172.19.0.2',
+  host: 'postgres',
   database: 'listinginfo',
   password: 'connorhoman',
   port: 5432,
