eb481e07d144294cf96c331f9e9868a182f82370
Connor Homan <connorhoman14@gmail.com>
2019-06-01 17:17:25 -0700

    Changed host...

--- database/queries.js
+++ database/queries.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'connorhoman',
-  host: '172.19.0.2',
+  host: '172.17.0.3',
   database: 'listinginfo',
   password: 'connorhoman',
   port: 5432,
